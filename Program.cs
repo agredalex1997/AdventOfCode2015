6492dafa21d106cebf6f761f0acb12fc237cb510
Alex Ágreda <agredalex1997@gmail.com>
2024-07-16 23:06:52 -0600

    Day1_Part2

--- Program.cs
+++ Program.cs
@@ -2,10 +2,17 @@
     using StreamReader reader = new("Day1/input.txt");
     string text = reader.ReadToEnd();
     int floor = 0;
+    int position = 0;
     foreach(char c in text) {
+        position += 1;
         floor += c == '(' ? 1 : -1;
+
+        if (floor == -1) {
+            Console.WriteLine($"Position: {position}");
+            break;
+        }
     }
-    Console.WriteLine($"Floor: {floor}");
+    // Console.WriteLine($"Floor: {floor}");
 } catch (Exception e) {
     Console.WriteLine(e.Message);
 }
