13e609772430eca48884a7397acd55f9c599e0e8
Alex Ágreda <agredalex1997@gmail.com>
2024-07-16 23:06:22 -0600

    Day1_Part1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿try {
+    using StreamReader reader = new("Day1/input.txt");
+    string text = reader.ReadToEnd();
+    int floor = 0;
+    foreach(char c in text) {
+        floor += c == '(' ? 1 : -1;
+    }
+    Console.WriteLine($"Floor: {floor}");
+} catch (Exception e) {
+    Console.WriteLine(e.Message);
+}
