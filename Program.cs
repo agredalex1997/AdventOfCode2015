48a0e5a26bdd9be46febc6de4a3a7f377dcb7618
Alex Ágreda <agredalex1997@gmail.com>
2024-07-16 23:42:23 -0600

    Day2_Part1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
-﻿try {
-    using StreamReader reader = new("Day1/input.txt");
-    string text = reader.ReadToEnd();
-    int floor = 0;
-    int position = 0;
-    foreach(char c in text) {
-        position += 1;
-        floor += c == '(' ? 1 : -1;
+﻿try
+{
+    using (StreamReader reader = new("Inputs/Day2.txt"))
+    {
+        int TotalArea = 0;
 
-        if (floor == -1) {
-            Console.WriteLine($"Position: {position}");
-            break;
+        while (reader.Peek() >= 0) {
+            string line = reader.ReadLine();
+            int[] dimensions = line.Split("x").Select(x => int.Parse(x)).Order().ToArray();
+            
+            int x = dimensions[0];
+            int y = dimensions[1];
+            int z = dimensions[2];
+
+            int boxSurfaceArea = 2*x*y + 2*x*z + 2*y*z;
+            // Since the length of the sides is in ascending order, the first 2 positions form the smallest side
+            int smallestSideArea = x * y;
+
+            TotalArea += boxSurfaceArea + smallestSideArea;
         }
+
+        Console.WriteLine($"Total area: {TotalArea}");
     }
-    // Console.WriteLine($"Floor: {floor}");
-} catch (Exception e) {
+}
+catch (Exception e)
+{
     Console.WriteLine(e.Message);
 }
