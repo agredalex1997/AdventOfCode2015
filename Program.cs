618813c6986e0a8c3ab38558910f2a24403fec72
Alex Ágreda <agredalex1997@gmail.com>
2024-07-18 22:39:57 -0600

    Day3_Part2

--- Program.cs
+++ Program.cs
@@ -1,89 +1,70 @@
-﻿using System.Text;
-
-try
+﻿namespace AdventOfCode
 {
-    using (StreamReader reader = new("Inputs/Day3.txt"))
+    class Position
     {
-        string moves = reader.ReadToEnd();
-
-        (int x, int y) position = (0, 0);
-        List<(int x, int y)> visitedPositions = [(0, 0)];
-        int minX = 0;
-        int maxX = 0;
-        int minY = 0;
-        int maxY = 0;
-
-        foreach (var move in moves)
+        public Position(int x, int y)
         {
-            if (move == '^')
-            {
-                position.y += 1;
-            }
-            else if (move == 'v')
-            {
-                position.y -= 1;
-            }
-            else if (move == '>')
-            {
-                position.x += 1;
-            }
-            else
-            {
-                position.x -= 1;
-            }
-
-            visitedPositions.Add(position);
-
-            if (position.x < minX)
-            {
-                minX = position.x;
-            }
-            else if (position.x > maxX)
-            {
-                maxX = position.x;
-            }
-
-            if (position.y < minY)
-            {
-                minY = position.y;
-            }
-            else if (position.y > maxY)
-            {
-                maxY = position.y;
-            }
+            X = x;
+            Y = y;
         }
 
-        var distinctVisitedPositions = visitedPositions.ToHashSet();
+        internal int X { get; set; }
+        internal int Y { get; set; }
 
-        Console.WriteLine($"minX: {minX} maxX: {maxX} minY: {minY} maxY: {maxY}");
-
-        List<(int x, int y)> path = new();
-
-        Console.Clear();
-        for (int i = minY; i < maxY; i++)
-        {
-            StringBuilder line = new();
-
-            for (int j = minX; j < maxX; j++)
-            {
-                line.Append('○');
-            }
-
-            line.Append('\n');
-            Console.Write(line);
+        public (int, int) ToTuple() {
+            return (X, Y);
         }
-
-        visitedPositions.ForEach(position =>
-        {
-            Console.SetCursorPosition(position.x - minX, position.y - minY);
-            Console.Write('●');
-            Thread.Sleep(25);
-        });
-
-        Console.ReadLine();
     }
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader reader = new("Inputs/Day3.txt"))
+                {
+                    string moves = reader.ReadToEnd();
+
+                    var santaPosition = new Position(0, 0);
+                    var roboSantaPosition = new Position(0, 0);
+                    HashSet<(int, int)> visitedPositions = [(0, 0)];
+
+                    for (int i = 0; i < moves.Length; i++)
+                    {
+                        char move = moves[i];
+                        var positionToUpdate = i % 2 == 0 ? santaPosition : roboSantaPosition;
+
+                        UpdatePosition(positionToUpdate, move);
+                        visitedPositions.Add(positionToUpdate.ToTuple());
+                    }
+
+                    Console.WriteLine($"Visited houses = {visitedPositions.Count}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void UpdatePosition(Position positionToUpdate, char move)
+        {
+            switch (move)
+            {
+                case '^':
+                    positionToUpdate.Y += 1;
+                    break;
+                case 'v':
+                    positionToUpdate.Y -= 1;
+                    break;
+                case '>':
+                    positionToUpdate.X += 1;
+                    break;
+                case '<':
+                    positionToUpdate.X -= 1;
+                    break;
+            }
+        }
+    }
 }
