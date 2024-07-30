6768a8b137c5c6cf4375ead3154ff6bc17cf2f7d
Alex Ágreda <agredalex1997@gmail.com>
2024-07-29 20:56:21 -0600

    Day8_Part1

--- Program.cs
+++ Program.cs
@@ -1,125 +1,37 @@
-﻿using System.Diagnostics.Tracing;
-using System.Text.Json;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
-    internal class Instruction
+    internal partial class Program
     {
-        public string variableName { get; set; }
-        public string[] assignmentTokens { get; set; }
-        public ushort? solution { get; set; } = null;
-
-        public Instruction(string varName, string[] tokens)
-        {
-            variableName = varName;
-            assignmentTokens = tokens;
-        }
-    }
-
-    internal class Program
-    {
-        private static List<Instruction> instructions = new();
         private static void Main(string[] args)
         {
             try
             {
-                using (StreamReader reader = new("Inputs/Day7.txt"))
+                using (StreamReader reader = new("Inputs/Day8.txt"))
                 {
+                    int countStringLiterals = 0;
+                    int countInMemory = 0;
+
                     while (reader.Peek() >= 0)
                     {
                         string? line = reader.ReadLine();
-                        string[] parts = line.Split(" -> ");
-                        string assignment = parts[0];
-                        string variableName = parts[1];
-
-                        instructions.Add(new Instruction(variableName, assignment.Split(' ')));
+                        countStringLiterals += line.Length;
+                        string parsedLine = Regex.Replace(line, "\\\\\\\\", ".");
+                        parsedLine = Regex.Replace(parsedLine, "\\\\x..", ".");
+                        parsedLine = Regex.Replace(parsedLine, "\\\\\"", ".");
+                        parsedLine = Regex.Replace(parsedLine, "\"", string.Empty);
+                        countInMemory += parsedLine.Length;
+                        Console.WriteLine($"{line} {line.Length}, {parsedLine} {parsedLine.Length}");
                     }
 
-                    Solve("a");
-
-                    // Console.Write(JsonSerializer.Serialize(instructions.Where(x => x.solution != null), new JsonSerializerOptions() { WriteIndented = true }));
+                    Console.WriteLine($"{countStringLiterals} - {countInMemory} = {countStringLiterals - countInMemory}");
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-        }
-
-        private static ushort Solve(string variable)
-        {
-
-            List<string> operators = new() { "AND", "OR", "NOT", "LSHIFT", "RSHIFT" };
-
-            Instruction instructionToSolve = instructions.Find(x => x.variableName == variable);
-
-            Console.WriteLine($"Solving {variable} = {string.Join(' ', instructionToSolve.assignmentTokens)}");
-
-            if (instructionToSolve.solution != null)
-            {
-                return (ushort)instructionToSolve.solution;
-            }
-            else
-            {
-                if (instructionToSolve.assignmentTokens.Length == 1)
-                {
-                    // 123 -> x
-                    if (ushort.TryParse(instructionToSolve.assignmentTokens[0], out ushort value))
-                    {
-                        instructionToSolve.solution = value;
-                    }
-                    // y -> x
-                    else
-                    {
-                        instructionToSolve.solution = Solve(instructionToSolve.assignmentTokens[0]);
-                    }
-                }
-                else if (instructionToSolve.assignmentTokens.Length == 2)
-                {
-                    // NOT 123 -> x
-                    if (ushort.TryParse(instructionToSolve.assignmentTokens[1], out ushort value))
-                    {
-                        instructionToSolve.solution = (ushort?)~value;
-                    }
-                    // NOT y -> x
-                    else
-                    {
-                        instructionToSolve.solution = (ushort?)~Solve(instructionToSolve.assignmentTokens[1]);
-                    }
-                }
-                else
-                {
-                    if (!ushort.TryParse(instructionToSolve.assignmentTokens[0], out ushort firstValue))
-                    {
-                        firstValue = Solve(instructionToSolve.assignmentTokens[0]);
-                    }
-
-                    if (!ushort.TryParse(instructionToSolve.assignmentTokens[2], out ushort secondValue))
-                    {
-                        secondValue = Solve(instructionToSolve.assignmentTokens[2]);
-                    }
-
-                    switch (instructionToSolve.assignmentTokens[1])
-                    {
-                        case "AND":
-                            instructionToSolve.solution = (ushort?)(firstValue & secondValue);
-                            break;
-                        case "OR":
-                            instructionToSolve.solution = (ushort?)(firstValue | secondValue);
-                            break;
-                        case "LSHIFT":
-                            instructionToSolve.solution = (ushort?)(firstValue << secondValue);
-                            break;
-                        case "RSHIFT":
-                            instructionToSolve.solution = (ushort?)(firstValue >> secondValue);
-                            break;
-                    }
-                }
-            }
-
-            Console.WriteLine($"{variable} = {instructionToSolve.solution}");
-
-            return (ushort)instructionToSolve.solution;
         }
     }
 }
